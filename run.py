4c36783f6a3ff008ab44cb957908d2207a26f410
Remi Tachet des Combes <remi.tachet@microsoft.com>
2018-03-05 15:51:20 -0500

    Stuff

--- run.py
+++ run.py
@@ -143,6 +143,7 @@ with tf.device('/gpu:0'):
     optimizer = tf.group(nn.adam_updates(all_params, grads, lr=tf_lr, mom1=0.95, mom2=0.9995), maintain_averages_op)
 
 # convert loss to bits/dim
+print(loss_gen)
 bits_per_dim = loss_gen[0]/(np.log(2.)*np.prod(obs_shape)*args.batch_size)
 bits_per_dim_test = loss_gen_test[0]/(np.log(2.)*np.prod(obs_shape)*args.batch_size)
 
