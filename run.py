ad9a0f8f5d4dc7229d85061eb74119db5e0f9eb1
Remi Tachet des Combes <remi.tachet@microsoft.com>
2018-04-09 12:56:21 -0400

    Test

--- run.py
+++ run.py
@@ -215,7 +215,6 @@ with tf.device('/gpu:0'):
     print(loss_gen_3)
 
     for g in grads_2[0]:
-        print(g)
         grad_to_be_used.append(tf.placeholder(dtype=tf.float32, shape=g.shape))
     # training op
     optimizer_2 = tf.group(nn.adam_updates(all_params, grad_to_be_used, lr=tf_lr, mom1=0.95, mom2=0.9995), maintain_averages_op)
