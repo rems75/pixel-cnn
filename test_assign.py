b5278e3f6f807472cb9d9b06e581a22c4040575a
Remi Tachet des Combes <remi.tachet@microsoft.com>
2018-05-01 11:30:00 -0400

    Test

--- test_assign.py
+++ test_assign.py
@@ -11,11 +11,12 @@ import tensorflow as tf
 with tf.device('/gpu:0'):
 
   t = tf.Variable([6, 120], name='yo')
-  t_2 = tf.Variable([6, 120], name='yo_1')
+  t_2 = tf.Variable([6, 120], name='yooo')
 
 with tf.Session(config=tf.ConfigProto(log_device_placement=True)) as sess:
 
   sess.run(tf.global_variables_initializer())
   begin = time.time()
+  print("Assigning")
   sess.run(t_2.assign(t))
   print("Assign in {} seconds".format(time.time() - begin))
